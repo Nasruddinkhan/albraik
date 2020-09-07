6fe07aa088a4a19c66382247355394df1133f077
Asif Shaikh <shaikhasif99@gmail.com>
2020-09-07 21:40:01 +0530

    Created User Registration for Admin and added Hijri calender class

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { ColorEvent } from 'ngx-color';
 
 @Component({
-  templateUrl: 'dashboard.component.html'
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-
+  
   ngOnInit(): void {
     // generate random values for mainChart
     
   }
+
+  
 }
--- src/app/views/users/user/user.component.ts
+++ src/app/views/users/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {NgbdDatepickerIslamiccivil} from '../datepicker-islamiccivil/datepicker-islamiccivil'
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
+  @ViewChild(NgbdDatepickerIslamiccivil) islamicCalendar; 
 
   constructor() { }
 
