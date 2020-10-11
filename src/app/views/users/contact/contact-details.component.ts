e2757c7d69619c555624a72645e87647e4e8eaaa
Asif Shaikh <shaikhasif99@gmail.com>
2020-10-11 22:14:06 +0530

    Change in Table

--- src/app/views/users/contact/contact-details.component.ts
+++ src/app/views/users/contact/contact-details.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-contact-details',
-    templateUrl: './contact-details.component.html'
+    templateUrl: './contact-details.component.html',
+    styleUrls: ['./contact-details.component.css']
 })
 export class ContactDetailsComponent implements OnInit{
 
